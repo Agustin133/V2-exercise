d5d8724b09e9d8da5ee8a486d349cf258bc0c3cb
Agustin133 <agustin5oberti@gmail.com>
2024-07-22 16:37:25 -0300

    fix report service

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -1,8 +1,42 @@
 import Transaction from '../models/transactionModel';
+import moment from 'moment';
 
-class Report{
-    async generateReport(start: Date, end: Date){
+class Report {
+    async generateReport(start: Date, end: Date) {
         try {
+            // Validate start and end dates
+            if (!(start instanceof Date) || isNaN(start.getTime())) {
+                throw new Error('Invalid start date');
+            }
+            if (!(end instanceof Date) || isNaN(end.getTime())) {
+                throw new Error('Invalid end date');
+            }
+            if (start > end) {
+                throw new Error('Start date must be before or equal to end date');
+            }
+
+            // Total credits
+            const totalCreditsResult = await Transaction.aggregate([
+                {
+                    $match: {
+                        date: { $gte: start, $lte: end },
+                    },
+                },
+                {
+                    $group: {
+                        _id: null,
+                        totalCredits: { $sum: '$credits' },
+                    },
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        totalCredits: 1,
+                    },
+                },
+            ]);
+
+            // Detailed transactions
             const transactions = await Transaction.aggregate([
                 {
                     $match: {
@@ -12,27 +46,34 @@ class Report{
                 {
                     $group: {
                         _id: { senderId: '$senderId', receiverId: '$receiverId' },
-                        totalCreditsSent: { $sum: { $cond: [{ $eq: ['$_id.senderId', '$_id.senderId'] }, '$credits', 0] } },
-                        totalCreditsReceived: { $sum: { $cond: [{ $eq: ['$_id.receiverId', '$_id.receiverId'] }, '$credits', 0] } },
+                        totalCreditsSent: { $sum: '$credits' },  // Correct summation
+                        totalCreditsReceived: { $sum: '$credits' },  // Correct summation
                     },
                 },
                 {
                     $project: {
                         _id: 0,
                         senderId: '$_id.senderId',
-                        receiverId: '$_id.receiverId',
                         totalCreditsSent: 1,
+                        receiverId: '$_id.receiverId',
                         totalCreditsReceived: 1,
                     },
                 },
             ]);
-    
-            //const totalCredits = transactions.length > 0 ? transactions[0].totalCredits : 0;
-        
-            return transactions
-    
+
+            const totalCredits = totalCreditsResult.length > 0 ? totalCreditsResult[0].totalCredits : 0;
+            const formattedStart = moment(start).format('MMMM Do YYYY');
+            const formattedEnd = moment(end).format('MMMM Do YYYY');
+
+            return {
+                totalCredits,
+                from: formattedStart,
+                to: formattedEnd,
+                transactions
+            };
+
         } catch (err) {
-            throw err; 
+            throw err;
         }
     }
 }
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -12,24 +12,6 @@ const handleError = (err: unknown, res: Response): void => {
         console.error('Unknown error occurred:', err);
         res.status(500).json({ error: 'Unknown error occurred' });
     }
-    // if (err instanceof Error) {F
-    //     if (err.message === 'User not found') {
-    //         res.status(404).json({ error: err.message });
-    //     } else if (err.message === 'There are users in the database') {
-    //         res.status(404).json({ error: err.message });
-    //     } else if (err.message === `Credits must be more than '0'` || err.message === 'Credits must be a number') {
-    //         res.status(400).json({ error: err.message });
-    //     } else if('User does not have enough credits.') {
-    //         res.status(404).json({ error: err.message });
-    //     }  else if('User does not exist..') {
-    //         res.status(404).json({ error: err.message });
-    //     }  else {
-    //         res.status(500).json({ error: err.message });
-    //     } 
-    // } else {
-    //     console.error('Unknown error occurred:', err);
-    //     res.status(500).json({ error: 'Unknown error occurred' });
-    // }
 };
 
 export default handleError;
