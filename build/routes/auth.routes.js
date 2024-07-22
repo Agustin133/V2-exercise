5452f24ae5a3b56a8e674f3defabe4c91899a3e3
Agustin133 <agustin5oberti@gmail.com>
2024-07-22 13:21:45 -0300

    Remove .env node_modules and build

--- build/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = (0, express_1.Router)();
-router.post('/signup', auth_controller_1.signUp);
-router.post('/signin', auth_controller_1.signIn);
-exports.default = router;
